2f9539c3957c7a6550e2f6edf02a4ed9ea5dec91
SUN06444 <sun.msc06444@gmail.com>
2018-05-01 09:54:12 +0800

    Ch7-2-2 建立選項選單(Menu)

---
+++ Ch7-2-2-Menu/app/src/main/java/com/example/admin/ch7_2_2_menu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.ch7_2_2_menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
