17de8251dcef6324f65a95538cbda52205f7cf0d
SUN06444 <sun.msc06444@gmail.com>
2018-05-08 11:09:25 +0800

    新增共兩個片段、透過button做add、replace、remove片段的動作

--- Ch7_5/app/src/main/java/com/example/admin/ch7_5/MainActivity.java
+++ Ch7_5/app/src/main/java/com/example/admin/ch7_5/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.admin.ch7_5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirstFragment First = new FirstFragment();
+    SecondFragment Second = new SecondFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, First).commit();
+    }
+
+    public void btn_replace(View v){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, Second).commit();
+    }
+
+    public void btn_delete(View v){
+        getSupportFragmentManager().beginTransaction().remove(Second).commit();
     }
 }
