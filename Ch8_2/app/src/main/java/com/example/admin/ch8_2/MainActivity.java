f780b6db19c5ca4c928c347529c1155521a872d4
SUN06444 <sun.msc06444@gmail.com>
2018-05-15 11:35:03 +0800

    單選對話方塊(single)+綁定事件

--- Ch8_2/app/src/main/java/com/example/admin/ch8_2/MainActivity.java
+++ Ch8_2/app/src/main/java/com/example/admin/ch8_2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.admin.ch8_2;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn_confirm = (Button)findViewById(R.id.btn_confirm);
         btn_confirm.setOnClickListener(btn_confirm_listener);
+
+        Button btn_single = (Button)findViewById(R.id.btn_single);
+        btn_single.setOnClickListener(btn_single_listener);
 
 
     }
@@ -60,6 +64,35 @@ public class MainActivity extends AppCompatActivity {
                     .setMessage("確認結束本程式")
                     .setPositiveButton("確定", dialog_Ok)
                     .setNegativeButton("取消", dialog_Cancel)
+                    .show();
+        }
+    };
+
+    DialogInterface.OnClickListener dialog_single_option = new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialogInterface, int i){
+            Button btn_single = (Button)findViewById(R.id.btn_single);
+            switch (i){
+                case 0:
+                    btn_single.setBackgroundColor(Color.RED);
+                    break;
+                case 1:
+                    btn_single.setBackgroundColor(Color.YELLOW);
+                    break;
+                case 2:
+                    btn_single.setBackgroundColor(Color.GREEN);
+                    break;
+            }
+        }
+    };
+
+    View.OnClickListener btn_single_listener = new View.OnClickListener(){
+        public void onClick(View v){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            String[] singleOptions = { "紅色", "黃色", "綠色" };
+            builder.setTitle("確認")
+                    .setItems(singleOptions,dialog_single_option)
+                    .setNegativeButton("取消", null)
+                    .create()
                     .show();
         }
     };
