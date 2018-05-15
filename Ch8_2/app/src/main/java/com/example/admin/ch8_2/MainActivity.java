5825c098c78052d5c47cf6c6257f252835aec7d0
SUN06444 <sun.msc06444@gmail.com>
2018-05-15 10:26:03 +0800

    非動態綁定事件(button_Onclick)

--- Ch8_2/app/src/main/java/com/example/admin/ch8_2/MainActivity.java
+++ Ch8_2/app/src/main/java/com/example/admin/ch8_2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.admin.ch8_2;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void button_Click(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("關於")
+                .setMessage("版本:7.0版 \n 作者:陳會安")
+                .setPositiveButton(
+                        "確定"
+                        , new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialogInterface, int i){
+
+                            }
+                        })
+                .show();
     }
 }
