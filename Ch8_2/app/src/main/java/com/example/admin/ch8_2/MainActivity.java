23983cb4ae7255298b0d4d578ed320eeb62ce90b
SUN06444 <sun.msc06444@gmail.com>
2018-05-15 10:30:31 +0800

    動態綁定事件(button_onClick) ->需使用MainActivity.this

--- Ch8_2/app/src/main/java/com/example/admin/ch8_2/MainActivity.java
+++ Ch8_2/app/src/main/java/com/example/admin/ch8_2/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_message = (Button)findViewById(R.id.btn_message);
+        btn_message.setOnClickListener(btn_message_listener);
     }
 
+    View.OnClickListener btn_message_listener = new View.OnClickListener(){
+        public void onClick(View v){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+            builder.setTitle("關於")
+                    .setMessage("版本:7.0版 \n 作者:陳會安")
+                    .setPositiveButton(
+                            "確定"
+                            , new DialogInterface.OnClickListener(){
+                                public void onClick(DialogInterface dialogInterface, int i){
+
+                                }
+                            })
+                    .show();
+        }
+    };
+
+    /*非動態綁定
     public void button_Click(View v){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -28,4 +50,5 @@ public class MainActivity extends AppCompatActivity {
                         })
                 .show();
     }
+    */
 }
